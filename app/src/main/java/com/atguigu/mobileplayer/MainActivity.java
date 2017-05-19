f2844c3648ca4fc87ed3c6c902c8c36ed3bf9eef
rx1996 <3343108599@qq.com>
2017-05-19 17:01:33 +0800

    第八次 视频列表显示

--- app/src/main/java/com/atguigu/mobileplayer/MainActivity.java
+++ app/src/main/java/com/atguigu/mobileplayer/MainActivity.java
@@ -1,5 +1,9 @@
 package com.atguigu.mobileplayer;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -94,6 +98,27 @@ public class MainActivity extends AppCompatActivity {
             tempFragment = currentFragment;
         }
     }
+    /**
+     * 解决安卓6.0以上版本不能读取外部存储权限的问题
+     * @param activity
+     * @return
+     */
+    public static boolean isGrantExternalRW(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && activity.checkSelfPermission(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            activity.requestPermissions(new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, 1);
+
+            return false;
+        }
+
+        return true;
+    }
+
+
     @Override
     protected void onRestart() {
         super.onRestart();
